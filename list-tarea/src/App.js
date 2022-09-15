44fb82c282b8e3ff5cbbc9e057525d783cb6e6e3
Jaime Urrego <or4846@gmail.com>
2022-09-14 19:14:14 -0500

    preba

--- list-tarea/src/App.js
+++ list-tarea/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>srcApp</code> and save to reload.
+          it <code>srcApp</code> and save to reload.
         </p>
         <a
           className="App-link"
